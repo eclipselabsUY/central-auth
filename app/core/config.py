632d1b4113860efba2ef662506eca0dd8ac25404
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-06 16:25:13 -0300

    Create config.py

---
+++ app/core/config.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os
+
+# Load Env Vars
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+dotenv_path = os.path.join(PROJECT_ROOT, ".env")
+load_dotenv(dotenv_path)
+
+# Keys and Passwords
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+
+if ENVIRONMENT == "DEV":
+    DATABASE_URL = "sqlite+aiosqlite:///./egos.db"
+else:
+    DATABASE_URL = (
+        f"postgresql+asyncpg://"
+        f"{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}"
+        f"/{POSTGRES_DB}"
+    )
