f2fc1951fe3f046aa4237556b9e1fbe2e25e33b8
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-06 16:24:51 -0300

    Migrations

---
+++ jetbase/env.py
@@ -0,0 +1,40 @@
+# Jetbase Configuration
+# Update the sqlalchemy_url with your database connection string.
+
+from dotenv import load_dotenv
+import os
+
+# Load Env Vars
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+dotenv_path = os.path.join(PROJECT_ROOT, ".env")
+load_dotenv(dotenv_path)
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+
+if ENVIRONMENT == "DEV":
+    DATABASE_URL = "sqlite+aiosqlite:///./egos.db"
+else:
+    DATABASE_URL = (
+        f"postgresql+asyncpg://"
+        f"{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}"
+        f"/{POSTGRES_DB}"
+    )
+
+if ENVIRONMENT == "DEV":
+    def get_env_vars():
+        return {
+            "sqlalchemy_url": DATABASE_URL,
+            "async_mode": True,
+        }
+else:
+    def get_env_vars():
+        return {
+            "sqlalchemy_url": DATABASE_URL,
+            "async_mode": True,
+        }
