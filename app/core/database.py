c46543ace1519140ed2e81ec7b9f87bad4a8a3ee
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-06 16:25:17 -0300

    Create database.py

---
+++ app/core/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import declarative_base
+from app.core.config import DATABASE_URL
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+Base = declarative_base()
