9392ff09771d8c213e8a447ab42e41d3ad251af8
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-10 16:31:46 -0300

    Update router.py

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Request
+from app.schemas.login import LoginSchema
 
 router = APIRouter()
 
 @router.get("/login")
-def login_request():
+def login_request(request : Request, login: LoginSchema):
     pass
