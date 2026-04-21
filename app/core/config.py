4481c0a35b2fa66c1ad1973a2136cac65013241e
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-04-21 16:09:22 -0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -22,7 +22,7 @@ PEPPER = os.getenv("PEPPER")
 
 # SMTP
 SMTP_HOST = os.getenv("SMTP_HOST")
-SMTP_PORT = os.getenv("SMTP_HOST")
+SMTP_PORT = os.getenv("SMTP_PORT")
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 SMTP_FROM = os.getenv("SMTP_FROM")
