e9218a0035cf2c4267644d8dc38a14b35b292fc9
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-04-21 16:09:54 -0300

    fix: centralised session

--- app/services/users.py
+++ app/services/users.py
@@ -1,11 +1,13 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends
+from uuid import UUID
 
 from app.models.user import User
-from app.core.database import get_async_session, 
 
 
-async def get_user(email : str, session : AsyncSession = Depends(get_async_session)) -> User:
+async def get_user(email : str, session : AsyncSession) -> User:
     result = await session.execute(select(User).where(User.email == email))
     return result.scalar_one_or_none
+
+async def update_user_hash(user_id : UUID, new_hash : str, session : AsyncSession):
+    await session.execute(update(User).where(User.id == user_id).values(password_hash = new_hash))
