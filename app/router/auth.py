8c40825c7ef8e2cf079e222512d708fe880c7e7f
Emiliano Gandini Outeda <104856039+emiliano-gandini-outeda@users.noreply.github.com>
2026-04-23 18:11:34 -0300

    Update auth.py

--- app/router/auth.py
+++ app/router/auth.py
@@ -2,25 +2,40 @@ from fastapi import APIRouter, Request, HTTPException, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.schemas.login import LoginSchema
-from app.services.auth import authenticate_user, check_verified, check_locked
-from app.services.users import get_user
+from app.services.users import get_user, validate_user
 from app.core.database import get_async_session
+from app.exceptions import UserLocked, UserNotVerified, UserWrongPassword
+from app.security.apikey import verify_api_key
 
 router = APIRouter()
 
 
-@router.post("/login")
+@router.post("/login", dependencies=(Depends(verify_api_key)))
 async def login_request(request: Request, login: LoginSchema, session : AsyncSession = Depends(get_async_session)):
 
     user = await get_user(login.email, session)
 
     if not user:
         raise HTTPException(status_code=401)
+    try:
     
-    is_user_auth = await authenticate_user(user.id, user.password_hash, login.password)
+        validate_user(user, login.password, session)
+    
+    except UserWrongPassword:
+        raise HTTPException(status_code=401, detail={
+            "error":"ACCOUNT_INVALID_CREDENTIALS",
+            "code":1101
+        })
+    
+    except UserNotVerified:
+        raise HTTPException(status_code=403, detail={
+            "error":"ACCOUNT_NOT_VERIFIED",
+            "code":1102            
+        })
+    
+    except UserLocked:
+        raise HTTPException(status_code=403, detail={
+            "error":"ACCOUNT_LOCKED",
+            "code":1103
+        })
 
-    if not is_user_auth:
-        raise HTTPException(status_code=401)
-    
-    check_verified(user.id)
-    check_locked(user.id)
